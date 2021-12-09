09b301cb3584a626719748cd828303d47df8f003
Unknown <bestbrett@aol.com>
2021-12-09 09:53:59 -1000

    planning for back end

--- back/server/index.js
+++ back/server/index.js
@@ -9,6 +9,47 @@ app.get('/', (req, res) => {
 });
 
 
+//#region user auth
+app.get('/auth', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/auth', (req, res) => {
+  res.send('Hello World');
+});
+//#endregion
+
+
+//#region chat
+app.get('/chat', (req, res) => {
+  res.send('Hello World');
+});
+
+app.post('/chat', (req, res) => {
+  res.send('Hello World');
+});
+//#endregion
+
+//#region calendar
+app.get('/calendar', (req, res) => {
+  res.send('Hello World');
+});
+//#endregion
+
+//#region users
+app.get('/users', (req, res) => {
+  res.send('Hello World');
+});
+//#endregion
+
+
+//#region video
+app.get('/video', (req, res) => {
+  res.send('Hello World');
+});
+//#endregion
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
---
+++ back/server/plannind.md
@@ -0,0 +1,27 @@
+
+post language and level
+connect through firebase database using config 
+send 201
+
+get language and level
+connect through firebase database using config
+send user language/level by searching firestore database collection by email, 200
+
+-----------------
+
+Chat: Jinho
+  POST req from front end (single line chat)
+  send text to Azure API to translate/ recieves translated text and OG text back
+  send translated text and OG text to Firebase message API
+  send front-end a 200
+  
+------------------
+
+Calendar: Carlos
+  POST req to create event on organizer calendar
+  POST req to invite attendees
+  
+
+--------
+### Video: Brett
+- create link
