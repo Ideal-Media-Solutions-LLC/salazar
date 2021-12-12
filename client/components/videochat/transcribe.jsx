21dbdaeb91dfed1c842120eb88be533c9f3ddde0
Unknown <bestbrett@aol.com>
2021-12-12 11:30:27 -1000

    more react porting work

--- client/components/videochat/transcribe.jsx
+++ client/components/videochat/transcribe.jsx
@@ -65,6 +65,49 @@ export default function Transcribe() {
 
   //#endregion
 
+  const RequestAuthorizationToken = function() {
+      if (authorizationEndpoint) {
+          var a = new XMLHttpRequest();
+          a.open("GET", authorizationEndpoint);
+          a.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+          a.send("");
+          a.onload = function () {
+              var token = JSON.parse(atob(this.responseText.split(".")[1]));
+              regionOptions.value = token.region;
+              authorizationToken = this.responseText;
+              key.disabled = true;
+              key.value = "using authorization token (hit F5 to refresh)";
+              console.log("Got an authorization token: " + token);
+          }
+      }
+  } 
+
+  // const Initialize = function(onComplete) {
+  //   if (!!window.SpeechSDK) {
+  //       document.getElementById('content').style.display = 'block';
+  //       document.getElementById('warning').style.display = 'none';
+  //       onComplete(window.SpeechSDK);
+  //   }
+  // }
+
+  const onClickScenarioStopButton = function(e) {
+    
+    reco.stopContinuousRecognitionAsync(
+      function () {
+        reco.close();
+        reco = undefined;
+      },
+      function (err) {
+        reco.close();
+        reco = undefined;
+      }
+    );
+  }
+
+
+
+  //#endregion
+
   // const RequestAuthorizationToken = function() {
   //   if (authorizationEndpoint) {
   //     var a = new XMLHttpRequest();
