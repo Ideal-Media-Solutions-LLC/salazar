4f8a0d8a7176ccdebcb40e59174c6e50868095c5
Jinho Kum <Jinho.kum@gmail.com>
2021-12-11 17:17:30 -0800

    work in progress for Azure translator

--- back/server/index.js
+++ back/server/index.js
@@ -37,15 +37,41 @@ app.get('/chat', (req, res) => {
   const getMessagesFromMe = await db.collection('messages').doc(req.query.sender_ID).where('user_id', '==', req.query.reciever_ID).get();
   const getMessagesFromOther = await db.collection('messages').doc(req.query.reciever_ID).where('user_id', '==', req.query.sender_ID).get();
 
-  res.send({
-    messagesFromMe: getMessagesFromMe,
-    messagesFromOther: getMessagesFromOther
-  });
+  //array in ordered time.
+  //{A: [all msg B sent to A]
+  // B: [all msg A sent to B]}
+  //ordered by timestamp
+  //earliest to latest
+  //[{A:{msg content}},{B: {msg content}},{A: msg content}]
+  var inOrderMsg = [];
+
+  var organize = function(indexMe, indexOther) {
+    if (getMessagesFromMe[indexMe] === undefined && getMessagesFromOther[indexOther] === undefined) {
+      return;
+    } else if (getMessagesFromMe[indexMe] === undefined) {
+      inOrderMsg.push(getMessagesFromOther[indexOther]);
+      organize(indexMe, indexOther + 1);
+    } else if (getMessagesFromOther[indexOther] === undefined) {
+      inOrderMsg.push(getMessagesFromMe[indexMe]);
+      organize(indexMe+1, indexOther);
+    } else if (getMessagesFromMe[indexMe].Time >= getMessagesFromOther[indexOther].Time) {
+      inOrderMsg.push(getMessagesFromMe[indexMe]);
+      organize(indexMe+1, indexOther);
+    } else {
+      inOrderMsg.push(getMessagesFromOther[indexOther]);
+      organize(indexMe, indexOther+1);
+    }
+  }
+
+  organize(0,0);
+
+  res.send(inOrderMsg);
 });
 
 app.post('/chat', (req, res) => {
   const getMessagesFromOther = await db.collection('messages').doc(req.body.reciever_ID).where('user_id', '==', req.query.sender_ID).get();
 
+  //{reviever_ID: sender_ID: {msg}}
   if (getMessagesFromOther) {
     db.collection('messages').doc(req.body.reciever_ID).update({
       req.body.sender_ID: FieldValue.arrayUnion({
@@ -75,6 +101,17 @@ app.post('/chat', (req, res) => {
   }
 
 });
+
+//azure translation
+const axios = require('axios');
+
+app.get('/chat/translation', (req, res) => {
+  const messages = await db.collection('messages').doc(req.body.sender_ID).where('user_id', '==', req.body.reciever_ID);
+  var promises = [];
+  for (var i = 0; i < messages.length; ++i) {
+    
+  }
+})
 //#endregion
 
 //#region calendar
