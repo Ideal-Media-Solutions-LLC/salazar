8f098d8cdd4745fb7c5cc7f293a3e9895d1a2087
Unknown <bestbrett@aol.com>
2021-12-13 08:24:27 -1000

    started token server route, more react porting
    added gitignore for azure api key file for video chat

--- back/server/index.js
+++ back/server/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const axios = require('axios');
 app.use(express.json());
 const port = require('../port.js');
+const api_z = require('../api_z.js');
 
 const { listEvents, createEvent } = require('../calendar.js');
 const { loadClient } = require('../googleCalApiClient.js');
@@ -235,6 +237,21 @@ app.get('/users', (req, res) => {
 
 //#region video
 app.get('/video', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/video/token', (req, res) => {
+  axios({
+    method: 'post',
+    url: 'https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken',
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': api_z
+    },
+    
+
+    
+  });
   res.send('Hello World');
 });
 //#endregion
--- client/components/videochat/onLoad.js
+++
@@ -1,7 +0,0 @@
-function Initialize(onComplete) {
-  if (!!window.SpeechSDK) {
-      document.getElementById('content').style.display = 'block';
-      document.getElementById('warning').style.display = 'none';
-      onComplete(window.SpeechSDK);
-  }
-}
