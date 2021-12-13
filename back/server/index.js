141e0e3dd5a172a1e7e82243091cbe47ba7a4cf0
Unknown <bestbrett@aol.com>
2021-12-13 08:48:14 -1000

    token function working, commented out firebase for my branch

--- back/server/index.js
+++ back/server/index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const port = require('../port.js');
 //import { route } from 'express/lib/application';
 //import { writeLanguages } from '../helpers.js';
-const firefunctions = require('../helpers.js');
+//const firefunctions = require('../helpers.js');
 const api_z = require('../api_z.js');
 
 app.get('/', (req, res) => {
@@ -78,15 +78,17 @@ app.get('/video/link', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/video/token', (req, res) => {
+app.post('/video/token', (req, res) => {
   axios({
     method: 'post',
     url: 'https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken',
     headers: {
       'Content-Type': 'application/json',
       'Ocp-Apim-Subscription-Key': api_z
-    },
-  }).then((response) => {res.send(response)})
+    }
+  }).then((response) => {
+    res.send(JSON.stringify(response.data))
+  })
   .catch((error => {
     console.log(error);
     res.sendStatus(500);
