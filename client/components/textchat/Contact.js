3318a1e4e078c2cd5b8cd42f104bf4021bcf1902
Viola Feng <avidafeng@gmail.com>
2021-12-14 10:41:53 -0800

    minor feature update

--- client/components/textchat/Contact.js
+++ client/components/textchat/Contact.js
@@ -7,10 +7,19 @@ import Divider from '@material-ui/core/Divider';
 
 
 const Contact = (props) => {
+
+  const {handleListItemClick, handleContactClick, selectedIndex} = props;
+
+
   return (
-    <ListItem button key={props.index} onClick={() => props.handleContactClick(props.contactId, '1')}>
+    <ListItem button key={props.index} onClick={() => {
+      handleContactClick(props.contactId, '1');
+      handleListItemClick(props.contactId);
+
+    }
+      }>
         <ListItemIcon>
-            <Avatar alt={props.name} src="https://material-ui.com/static/images/avatar/1.jpg" />
+            <Avatar selected={selectedIndex === props.contactId} alt={props.name} src="https://material-ui.com/static/images/avatar/1.jpg" />
         </ListItemIcon>
         <ListItemText primary={props.name}>{props.name}</ListItemText>
         <Divider />
