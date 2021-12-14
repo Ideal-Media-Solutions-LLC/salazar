179538a29c12004825ba488e2b460eaaa1817127
Xinyi Li <xinyil8023@gmail.com>
2021-12-14 14:42:18 -0800

    css modified

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import '../styles/carousel.css'
 import '../styles/signup.css'
 import { appWithTranslation } from 'next-i18next';
-import { AppWrapper } from '../components/context/State.js';
+import { AppWrapper } from '../components/context/AppProvider';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
