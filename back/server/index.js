c5065c34e9a92cc933a6634c814b7d5b93f42544
Alice Li <oceanylee@gmail.com>
2021-12-15 10:51:35 -0800

    Debug

--- back/server/index.js
+++ back/server/index.js
@@ -14,7 +14,7 @@ const { loadClient } = require('../googleCalApiClient.js');
 
 const app = express();
 app.use(express.json());
-
+app.use(cors);
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
